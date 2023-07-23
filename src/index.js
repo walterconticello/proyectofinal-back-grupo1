7259f2121b803b7955763a441e587e11beb2743d
mwarcos <mmwarcos@gmail.com>
2023-07-23 10:05:11 -0300

    cree la branch de productos

--- src/index.js
+++ src/index.js
@@ -3,23 +3,20 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 
-
-
 const app = express();
-
 
 app.set("port", process.env.PORT || 5500);
 
-
 const initApp = async () => {
   try {
-    app.listen(app.get("port"), () => {
-    console.log(`Backend conectado al puerto: ${app.get("port")}`);
-  })
-  .on("error", (error) => {
-    console.log("ERROR:", error);
-    process.exit(1);
-  });
+    app
+      .listen(app.get("port"), () => {
+        console.log(`Backend conectado al puerto: ${app.get("port")}`);
+      })
+      .on("error", (error) => {
+        console.log("ERROR:", error);
+        process.exit(1);
+      });
   } catch (error) {
     console.log("ERROR:", error);
     process.exit(1);
@@ -30,14 +27,11 @@ initApp();
 
 //MIDDLEWARE
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(morgan("dev")); 
-app.use(cors()); 
-
+app.use(morgan("dev"));
+app.use(cors());
 
 // Descomentar cuando tengamos las rutas
 // app.use("/api", require("./routes/Rutes"));
-
-
