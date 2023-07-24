8241c8ddd1f15ae1725a842eb6eba245830b2daf
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-07-24 15:56:34 -0300

    Se complejizo el schema de fields y se lo unio a un modelo

--- src/models/fields.model.js
+++ src/models/fields.model.js
@@ -3,10 +3,46 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const fieldsSchema = new Schema({
-    _idField: ObjectId,
-    name: String,
-    openHour: Date,
-    closeHour: Date,
-    pricePerHour: Number,
-    _idSportCenter: ObjectId
+    idField: {
+        type: ObjectId,
+        required: true,
+        index: true,
+        unique: true,
+        immutable: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 50
+    },
+    openHour: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 23,
+    },
+    closeHour: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 23,
+    },
+    pricePerHour: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100000
+    },
+    idSportCenter: {
+        type: ObjectId,
+        required: true,
+        immutable: true
+    }
 }, {versionKey: false});
+
+const fieldModel = mongoose.model("fields", fieldsSchema);
+
+export default fieldModel;
