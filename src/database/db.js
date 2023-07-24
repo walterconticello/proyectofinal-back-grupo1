abf1565d740f4335b0403a030af63c3c54f0791c
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-07-24 15:18:05 -0300

    Se creo el archivo de conexion de la base de datos

---
+++ src/database/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const URI = process.env.URI; //Getting URI from .env
+const DB = process.env.DB; //Getting database name from .env
+
+const connectDB = async () => {
+
+}
