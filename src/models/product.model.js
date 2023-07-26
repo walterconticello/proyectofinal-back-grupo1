09fb2591ac916c4a571480cc028d8287ed7640e0
mwarcos <mmwarcos@gmail.com>
2023-07-26 08:27:32 -0300

    se añadieron restricciones de seguridad

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -6,18 +6,24 @@ const productSchema = new Schema(
     name: {
       type: "string",
       required: true,
+      min: 3,
+      max: 45,
     },
     description: {
       type: "string",
       required: true,
+      min: 5,
+      max: 255,
     },
     price: {
       type: "number",
       required: true,
+      min: 0,
     },
     stock: {
       type: "number",
       required: true,
+      min: 0,
     },
   },
   { versionKey: false }
