2f57ea42dcfa5d9647ed9dec583c6061b4cdfbc2
Walter Conticello <walterconticellomsmt@gmail.com>
2023-07-26 16:19:03 -0300

    creo funcion de login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
 	try {
@@ -17,6 +18,26 @@ export const register = async (req, res, next) => {
 
 		await newUser.save()
 		res.status(200).send("El usuario fue creado!")
+	} catch (err){
+		next(err);
+	}
+}
+
+import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+
+export const login = async (req, res, next) => {
+	try {
+
+		const user = await User.findOne({email: req.body.email})
+		if (!user) return next(createError(404, "Usuario no encontrado!"));
+
+		const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+		if(!isPasswordCorrect) return next(createError(400, "Usuario o contraseña incorrectos!"))
+
+
+		const {password, isAdmin, ...otherDetails} = user._doc;
+		res.status(200).json({...otherDetails})
 	} catch (err){
 		next(err);
 	}
