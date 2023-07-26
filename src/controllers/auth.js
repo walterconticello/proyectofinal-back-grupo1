51934e36d411a4cd3356840f45ddd7e2a08d6ff6
Walter Conticello <walterconticellomsmt@gmail.com>
2023-07-26 16:09:22 -0300

    integracion de bcrypt y hash de password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,16 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res, next) => {
 	try {
+
+		const salt = bcrypt.genSaltSync(10);
+		const hash = bcrypt.hashSync(req.body.password, salt);
+
 		const newUser = new User({
 			username: req.body.username,
 			email: req.body.email,
-			password: req.body.password,
+			password: hash,
 			name: req.body.name,
 			lastname: req.body.lastname,
 		})
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { register } from "../controllers/auth";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/register", register)
+router.post("/register", register)
 
 export default router
