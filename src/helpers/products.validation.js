719a2a1b285b5cd4172b32dd931531532c08aef5
mwarcos <mmwarcos@gmail.com>
2023-07-28 08:37:47 -0300

    se añadio una nueva validacion para el stock

--- src/helpers/products.validation.js
+++ src/helpers/products.validation.js
@@ -7,6 +7,10 @@ const priceValidation = (price) => {
   return typeof price === "number" && price >= 0 && price <= 100000;
 };
 
+const stockValidation = (stock) => {
+  return typeof stock === "number" && stock >= 0 && stock <= 10000;
+};
+
 const createProductDataValidation = (product) => {
   const keys = Object.keys(product);
   for (let i = 0; i < keys.length; i++) {
