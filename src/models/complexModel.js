dea6ccad0ec94fe9afb43e33206a2e2fc21f5c6c
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-07-28 17:36:05 -0300

    Complex model completo

---
+++ src/models/complexModel.js
@@ -0,0 +1,34 @@
+import mongoose, { Mongoose, version } from "mongoose";
+
+const {schema}=mongoose;
+
+const complexSchema = new schema({   
+    name: { type: String, 
+        required: true,
+        trim: true,
+        lowercase: true,
+        minlenght: 3,
+        maxlenght: 30,
+    },
+        address: { type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            minlenght: 3,
+            maxlenght: 30,
+        },
+
+    idComplex: { type: Mongoose.types.ObjectId,
+        required: true,
+        inmutable: true,
+        default:new Mongoose.types.ObjectId(),
+    }
+}
+, {versionKey: false});
+
+const complexModel = mongoose.model("complex", complexSchema);
+
+export default mongoose.model("complex", complexSchema);
+
+
+
