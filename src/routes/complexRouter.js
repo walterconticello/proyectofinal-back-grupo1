58d6feb1ecfd3562e05bbb8153c73d5cd60eac80
Pablo Matias Rodriguez <Pablomatiasrodriguez95@gmail.com>
2023-07-28 19:26:23 -0300

    Completando complex routs

--- src/routes/complexRouter.js
+++ src/routes/complexRouter.js
@@ -1,3 +1,12 @@
 import { Express } from "express";
 import { ComplexController } from "../controllers/complexController";
 
+const router= express.Router(); 
+
+router.get("/", ComplexController.getComplex);
+router.post("/", ComplexController.postComplex);
+router.put("/:id", ComplexController.putComplex);
+router.delete("/:id", ComplexController.deleteComplex);
+
+module.exports = router;
+//
