93324b849d0d5e09edb75ef265804ea12859542c
mwarcos <mmwarcos@gmail.com>
2023-07-29 11:54:06 -0300

    cree las validaciones de las categorias

--- src/helpers/products.validation.js
+++ src/helpers/products.validation.js
@@ -4,8 +4,29 @@ const nameValidation = (name) => {
 };
 
 const descriptionValidation = (description) => {
-  const regex = /^[ A-Za-z\u00C0-\u024F0-9,.\:\(\)\[\]\'\"\`\n\r]+$/; //allows any character, including newlines and special characters.
+  const regex = /^[ A-Za-z\u00C0-\u024F0-9,.\:\(\)\[\]\'\"\`\n\r]+$/;
   return regex.test(description);
+};
+
+const categoriesValidation = (categories) => {
+  const Allcategories = [
+    "Balones",
+    "Calzado",
+    "Ropa",
+    "Entrenamiento",
+    "Accesorios",
+  ];
+
+  // Verificar si todas las categorías proporcionadas existen en el array de categorías válidas
+  const invalidCategories = categories.filter(
+    (category) => !Allcategories.includes(category)
+  );
+
+  // Devolver un objeto con información sobre si las categorías son válidas y cuáles son inválidas
+  return {
+    valid: invalidCategories.length === 0,
+    invalidCategories: invalidCategories,
+  };
 };
 
 const priceValidation = (price) => {
