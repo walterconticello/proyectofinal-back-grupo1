2541f3e74b152b8dbe796b747ca6e8cefc13ef15
mwarcos <mmwarcos@gmail.com>
2023-08-01 08:49:01 -0300

    cambie la forma de exportar e importar el auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,54 +1,57 @@
 import User from "../models/User.model.js";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 import { verifyToken } from "../utils/verifyToken.js";
 
-export const register = async (req, res, next) => {
-	try {
-		const salt = bcrypt.genSaltSync(10);
-		const hash = bcrypt.hashSync(req.body.password, salt);
+const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
-		const newUser = new User({
-			username: req.body.username,
-			email: req.body.email,
-			password: hash,
-		})
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
 
-		await newUser.save()
-		res.status(200).send("El usuario fue creado!");
-	} catch (err) {
-		next(err);
-	}
+    await newUser.save();
+    res.status(200).send("El usuario fue creado!");
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const login = async (req, res, next) => {
-	try {
-		const user = await User.findOne({ username: req.body.username })
-		if (!user) return next(createError(404, "Usuario no encontrado!"))
+const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "Usuario no encontrado!"));
 
-		const isPasswordCorrect = await bcrypt.compare(
-			req.body.password,
-			user.password
-		);
-		if (!isPasswordCorrect)
-			return next(createError(400, "Usuario o contraseña incorrectos!"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createError(400, "Usuario o contraseña incorrectos!"));
 
-		const token = jwt.sign(
-			{ id: user._id, isAdmin: user.isAdmin },
-			process.env.JWT
-		);
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
 
-		const { password, isAdmin, ...otherDetails } = user._doc;
-		res
-			.cookie("access_token", token, {
-				httpOnly: true,
-			})
-			.status(200)
-			.json({ ...otherDetails })
-
-	} catch (err) {
-		next(err);
-	}
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...otherDetails });
+  } catch (err) {
+    next(err);
+  }
 };
 
+module.exports = {
+  register,
+  login,
+};
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { login, register } from "../controllers/auth.controller.js";
 
+const { login, register } = require("../controllers/auth.controller.js");
+
 const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
+router.post("/register", register);
+router.post("/login", login);
 
-export default router
+export default router;
