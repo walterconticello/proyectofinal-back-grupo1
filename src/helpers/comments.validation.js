660dbd4488ba587f9c07f04f3ed303fdec498441
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-01 13:40:38 -0300

    Hecha la validacion de rating

--- src/helpers/comments.validation.js
+++ src/helpers/comments.validation.js
@@ -2,4 +2,8 @@ const textValidation = (text) => {
     return (typeof text === 'string') && text.length >= 5 && text.length <= 500;
 }
 
+const ratingValidation = (rating) => {
+    return (typeof rating === 'number') && (rating >= 0) && (rating <= 5);
+}
+
 export default {textValidation};
