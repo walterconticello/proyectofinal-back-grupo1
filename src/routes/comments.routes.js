6949e4a0f640bb318a36c4074ddb800c8610d970
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-08-01 13:32:37 -0300

    Se hicieron las rutas

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -4,5 +4,6 @@ import commentsController from "../controllers/comments.controller.js";
 const router = express.Router();
 
 router.get("/comments", commentsController.getAllComments);
+router.get("/comments/:id", commentsController.getByID);
 
 export default router;
