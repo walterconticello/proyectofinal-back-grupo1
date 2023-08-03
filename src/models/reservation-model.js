f9c5b3a248ab16d2fb9a001f3e6ff705510bd02d
Lautaro Cruz Srur <lautaro.cruzsrur@gmail.com>
2023-08-02 21:05:48 -0300

    pasando a import

--- src/models/reservation-model.js
+++ src/models/reservation-model.js
@@ -5,7 +5,7 @@ const reservationSchema = new Schema({
     IdUser : {type: String, require } ,
     IdSportCenter : { type: String , require },
     IdField : { type: String, require  },
-    ReservationTime: { type: String, require  }
+    ReservationTime: { type: Date, require  }
 }, {versionKey : false});
 
 export default ReservationModel = mongoose.model("reservations", reservationSchema); //Creamos la coleccion en la base de datos
