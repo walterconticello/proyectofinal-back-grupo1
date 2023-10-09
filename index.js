863e91fc5846328169534ad198f21a67d832acb5
Walter Conticello <walterconticellomsmt@gmail.com>
2023-10-08 21:07:08 -0300

    final deploy

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cors(
   {
-    origin: [],
+    origin: ["https://proyectofinal-back-grupo1.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
   }
