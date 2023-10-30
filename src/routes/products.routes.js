21eff7364a142881cd640514f9d360d8409fbb1c
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2023-10-30 14:24:13 -0300

    Update products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,19 +3,19 @@ const router = express.Router();
 import { verifyAdmin } from "../utils/verifyToken.js";
 import productsController from "../controllers/products.controller.js";
 
-//GET /products
+
 router.get("/products", productsController.getAllProducts);
 
-//GET BY ID /products/:id
+
 router.get("/products/:id", productsController.getProductById);
 
-//POST /products
+
 router.post("/products", verifyAdmin, productsController.createProduct);
 
-//PUT /products/:id
+
 router.put("/products/:id", verifyAdmin, productsController.updateProduct);
 
-//DELETE /products/:id
+
 router.delete("/products/:id", verifyAdmin, productsController.deleteProduct);
 
 export default router;
