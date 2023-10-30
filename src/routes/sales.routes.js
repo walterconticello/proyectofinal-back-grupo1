2b18eeeb7e3380e8cdb7985c694971cc682fc9dd
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2023-10-30 14:24:27 -0300

    Update sales.routes.js

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -4,15 +4,14 @@ const router = express.Router();
 import salesController from "../controllers/sales.controller.js";
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
-//POST /products
+
 router.post("/sales", verifyUser, salesController.createSale);
 
-//GET /sales
+
 router.get("/sales", verifyAdmin, salesController.getSales); //only admin
 
-//GET BY ID /products/:id
+
 router.get("/sales/:id", verifyAdmin, salesController.getSaleById);
 
-//GET BY userId
 
 export default router;
